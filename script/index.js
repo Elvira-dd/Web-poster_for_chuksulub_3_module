00c2f91f43636b154909c264fbe3752186f2b19a
Elvira-dd <elvira.6.galieva@yandex.ru>
2023-03-05 17:50:44 +0300

    two_screen_comp

--- script/index.js
+++ script/index.js
@@ -7,10 +7,12 @@ setTimeout(() =>{typeWriter(nextScreen_p)},5500)
 setTimeout( () => {document.getElementById("nextScreen").style.visibility="visible"}, 5500)
 
 
+
 $("#nextScreen").click(function(){
   screen_educ_login_1.style.visibility = "hidden"
   user_name.style.visibility = "hidden"
   nextScreen.style.visibility = "hidden"
+  nextScreen_p.style.visibility = "hidden"
   let userName = document.getElementById("user_name")
   let screenEduc = document.getElementById("screen_educ")
   let userNameValue = userName.value
@@ -20,14 +22,19 @@ $("#nextScreen").click(function(){
     console.log("Hide")
     let output = ""
     output = `<p id="elementEl" style="visibility:hidden";>Добро пожаловать, ${userNameValue}!
-     Теперь это ваше рабочее место. Сегодня ваша задача - пройти обучение и изучить механику работы>. 
+     Теперь это ваше рабочее место. Сегодня ваша задача - пройти обучения, изучить механику работы и обработать 3 запроса с планет А-564, З-98 и О-09 
     </p>`;
     document.getElementById("second_educ_screen").innerHTML = output;
     typeWriter(elementEl)
-    const elementPs = window.getComputedStyle(elementEl,"::after")
-    setTimeout(()=> {elementPs.getPropertyValue("display")="none"}, 7000)
-    setTimeout( () => {typeWriter(screen_educ_text_1)}, 12000)
-    setTimeout( () => {typeWriter(screen_educ_text_2)}, 15500)
+    // setTimeout(()=> {elementPs.getPropertyValue("display")="none"}, 7000)
+    setTimeout( () => {typeWriter(screen_educ_text_1)}, 11000)
+    setTimeout( () => {typeWriter(screen_educ_text_2)}, 13000)
+    setTimeout( () => {document.getElementById("screen_educ_tip").style.visibility="visible"}, 13000)
+    setTimeout( () => {typeWriter(screen_educ_text_3)}, 14500)
+    setTimeout( () => {document.getElementById("screen_educ_buttonYes").style.visibility="visible"}, 15500)
+    setTimeout( () => {typeWriter(screen_educ_buttonYes_p)}, 15500)
+    setTimeout( () => {document.getElementById("screen_educ_buttonNo").style.visibility="visible"}, 16000)
+    setTimeout( () => {typeWriter(screen_educ_buttonNo_p)}, 16000)
   }
   else {
     // supportSemmiNaming
