dc4350c96756e90fafa7e4b9b7e6699e12ab938b
Elvira-dd <elvira.6.galieva@yandex.ru>
2023-03-15 21:22:49 +0300

    lylylylylylyy

---
+++ script/tips.js
@@ -0,0 +1,40 @@
+$("#resque_screen_tip_1").click(function () {
+  $("#resque_screen_tip_box_1").css("visibility", "visible");
+  $(".hidden_tool").each(function () {
+    if ($(this).hasClass("tooltip_1")) {
+      $(this).css("opacity", "1");
+    } else {
+      $(this).css("opacity", ".4");
+    }
+  });
+  $(".menu_block").css("pointer-events", "none");
+});
+$(".closing_tip").click(function () {
+  $(".tip_boxes").css("visibility", "hidden");
+  $(".hidden_tool").each(function () {
+    $(this).css("opacity", "1");
+  });
+  $(".menu_block").css("pointer-events", "visible");
+});
+$("#resque_screen_tip_2").click(function () {
+  $("#resque_screen_tip_box_2").css("visibility", "visible");
+  $(".hidden_tool").each(function () {
+    if ($(this).hasClass("tooltip_2")) {
+      $(this).css("opacity", "1");
+    } else {
+      $(this).css("opacity", ".4");
+    }
+  });
+  $(".menu_block").css("pointer-events", "none");
+});
+$("#planet_screen_tip_1").click(function () {
+  $("#planet_screen_tip_box_1").css("visibility", "visible");
+  $(".hidden_tool").each(function () {
+    if ($(this).hasClass("tooltip_1_1")) {
+      $(this).css("opacity", "1");
+    } else {
+      $(this).css("opacity", ".4");
+    }
+  });
+  $(".menu_block").css("pointer-events", "none");
+});
